10648417db96f577731b9b628cc8409c0dbbaec8
kvohra <kvohra@appthority.com>
2016-04-13 13:13:28 -0700

    Use SSL and the Puma webserver in producation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ before_save { self.email = email.downcase }
   validates(:email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false})
   has_secure_password
   validates(:password, presence: true, length: {minimum: 6})
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> sign-up
+
